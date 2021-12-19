153e31228f98af48a67a92fc38c10036ef30cff8
meyrambek11 <m_kadyrbekov@bk.ru>
2021-12-19 23:01:27 +0600

    my first js adding

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(request, response){
+    response.send("<h1>Главная страница</h1>");
+});
+
+app.get("/about", function(request, response){ 
+    response.send("<h1>О сайте</h1>");
+});
+
+app.get("/contact", function(request, response){ 
+    response.send("<h1>контакты</h1>");
+});
+app.listen(3000);
